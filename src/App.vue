bc8a2815dfff2e7c6f83b18fde69dd113592ce5d
DrKillex <lcollaro1994@gmail.com>
2023-03-23 17:44:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  contenuto
+</template>
+
+<style lang="scss">
+@import "./components/common.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,104 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            navList: [
+                {
+                    tag: "CHARACTERS",
+                    link: "#"
+                },
+                {
+                    tag: "COMICS",
+                    link: "#"
+                },
+                {
+                    tag: "MOVIES",
+                    link: "#"
+                },
+                {
+                    tag: "TV",
+                    link: "#"
+                },
+                {
+                    tag: "GAMES",
+                    link: "#"
+                },
+                {
+                    tag: "COLLECTIBLES",
+                    link: "#"
+                },
+                {
+                    tag: "VIDEOS",
+                    link: "#"
+                },
+                {
+                    tag: "FANS",
+                    link: "#"
+                },
+                {
+                    tag: "NEWS",
+                    link: "#"
+                },
+                {
+                    tag: "SHOP",
+                    link: "#"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="src\assets\img\dc-logo.png" alt="">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li v-for="item in navList">
+                        <a href="item.link">{{ item.tag }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+
+    .logo {
+        width: 80px;
+        height: 80px;
+
+        img {
+            width: 100%;
+        }
+
+    }
+
+    .nav {
+        ul {
+            display: flex;
+            list-style: none;
+
+            li {
+                display: inline-block;
+                padding: 15px;
+
+                a {
+                    text-decoration: none;
+                    color: black;
+                }
+            }
+
+        }
+    }
+}</style>
